bbd913773e4869a3de1074cd33329e45963f2310
Tchoow <theo.lavie@live.fr>
2021-12-03 19:29:38 +0100

    Correction bug

--- script.js
+++ script.js
@@ -13,10 +13,14 @@
 // Ajout d'un event clique droit sur toute les cases
 // 🏳️
 // 💣
+
 const gamesize = 14;
 var tableauJeu = [];
-for (var i = 0; i < gamesize; i++) tableauJeu[i] = [];
+var nbCliques  =  0;
 
+
+for (var i = 0; i < gamesize; i++) tableauJeu[i] = [];
+const wrapper = document.querySelector('body');
 
 function initDefaultTableJeu() {
     for (var i = 0; i < gamesize; i++) {
@@ -32,7 +36,7 @@ function initDefaultTableJeu() {
     }
 }
 
-function initBombeTableJeu() {
+function initBombeTableJeu(row, col) {
     var nbBombe = Math.floor(gamesize * gamesize / 5);
     var nbBombePlaced = 0;
     while (nbBombePlaced < nbBombe) {
@@ -43,6 +47,25 @@ function initBombeTableJeu() {
             nbBombePlaced++;
         }
     }
+
+    tableauJeu[row][col].isMine  = false;
+    tableauJeu[row][col].nbMines = 0;
+    tableauJeu[row+1][col].isMine  = false;
+    tableauJeu[row+1][col].nbMines = 0;
+    tableauJeu[row][col+1].isMine  = false;
+    tableauJeu[row][col+1].nbMines = 0;
+    tableauJeu[row-1][col].isMine  = false;
+    tableauJeu[row-1][col].nbMines = 0;
+    tableauJeu[row][col-1].isMine  = false;
+    tableauJeu[row][col-1].nbMines = 0;
+    tableauJeu[row+1][col+1].isMine  = false;
+    tableauJeu[row+1][col+1].nbMines = 0;
+    tableauJeu[row-1][col-1].isMine  = false;
+    tableauJeu[row-1][col-1].nbMines = 0;
+    tableauJeu[row+1][col-1].isMine  = false;
+    tableauJeu[row+1][col-1].nbMines = 0;
+    tableauJeu[row-1][col+1].isMine  = false;
+    tableauJeu[row-1][col+1].nbMines = 0;
 }
 
 function getBombesArround() {
@@ -71,6 +94,7 @@ function revealBomnbeArround(x, y) {
     if (tableauJeu[x][y].isRevealed) return;
     if (tableauJeu[x][y].isMine) {
         revealAll();
+        wrapper.style.background = "#e74c3c";
         return;
     }
     tableauJeu[x][y].isRevealed = true;
@@ -92,6 +116,36 @@ function revealBomnbeArround(x, y) {
 
 }
 
+function styleUpdater(i, j) {
+
+  switch (tableauJeu[i][j].nbMines) {
+    case 1:
+      document.getElementById(i + "-" + j).style.color = "#2c3e50";
+      break;
+    case 2:
+      document.getElementById(i + "-" + j).style.color = "#27ae60";
+      break;
+    case 3:
+      document.getElementById(i + "-" + j).style.color = "#e67e22";
+      break;
+    case 4:
+      document.getElementById(i + "-" + j).style.color = "#e74c3c";
+      break;
+    case 5:
+      document.getElementById(i + "-" + j).style.color = "#9b59b6";
+      break;
+    case 6:
+      document.getElementById(i + "-" + j).style.color = "#f1c40f";
+      break;
+    case 7:
+      document.getElementById(i + "-" + j).style.color = "#1abc9c";
+      break;
+    case 8:
+      document.getElementById(i + "-" + j).style.color = "#34495e";
+      break;
+  }
+}
+
 function showTableJeu() {
     for (var i = 0; i < gamesize; i++) {
         for (var j = 0; j < gamesize; j++) {
@@ -107,7 +161,7 @@ function showTableJeu() {
                     
                       document.getElementById(i + "-" + j).innerHTML = tableauJeu[i][j].nbMines;
                       document.getElementById(i + "-" + j).style.background = "#b2bec3";
-                      document.getElementById(i + "-" + j).style.color = "#2c3e50";
+                      styleUpdater(i, j);
                     }
                 }
             } else {
@@ -123,6 +177,8 @@ function showTableJeu() {
 }
 
 
+
+
 // sheat mode who reveal all the table
 function revealAll() {
     for (var i = 0; i < gamesize; i++) {
@@ -133,14 +189,6 @@ function revealAll() {
     showTableJeu();
 }
 
-
-
-
-initDefaultTableJeu();
-initBombeTableJeu();
-getBombesArround();
-
-
 // Reveal Bombes
 const cases = document.querySelectorAll(".case");
 for(var i = 0; i < cases.length; i++) {
@@ -148,7 +196,26 @@ for(var i = 0; i < cases.length; i++) {
     var c = event.target.id.split("-");
     var row = parseInt(c[0]);
     var col = parseInt(c[1]);
-    revealBomnbeArround(row, col);
+    // Reveal the bomb if the case has no flag
+
+    if ( nbCliques == 0 ) {
+      initDefaultTableJeu();
+      initBombeTableJeu(row, col);
+      getBombesArround();
+      revealBomnbeArround(row, col);
+      console.log("Je passerais ici");
+    }
+    else {
+      if (!tableauJeu[row][col].isFlagged) {
+        revealBomnbeArround(row, col);
+      }
+    } 
+
+
+
+    //console.log(nbCliques);
+
+    nbCliques++;
     showTableJeu();
   });
 }
@@ -160,206 +227,14 @@ for(var i = 0; i < cases.length; i++) {
     var c = event.target.id.split("-");
     var row = parseInt(c[0]);
     var col = parseInt(c[1]);
-    tableauJeu[row][col].isFlagged = true;
+    if (!tableauJeu[row][col].isFlagged) {
+      tableauJeu[row][col].isFlagged = true;
+    }
+    else {
+      tableauJeu[row][col].isFlagged = false;
+    }
+    
     showTableJeu();
     event.preventDefault();
   });
 }
-
-
-
-/*
-
-function initBombe() {
-  for (var i = 0; i < gamesize; i++) {
-    for (var y = 0; y < gamesize; y++) {
-        if ( Math.floor(Math.random() * 6) == 1) tableauJeu[i][y] = "B";
-        else tableauJeu[i][y] = "0";
-
-
-    }
-  }
-}
-
-function initNumbers() {
-  for (var i = 0; i < gamesize; i++) {
-    for (var y = 0; y < gamesize; y++) {
-      if ( tableauJeu[i][y] !== "B") {
-        var nbBombeAutour = 0;
-        
-          if (y > 0) {
-            if ( tableauJeu[i][y+1]   == "B" ) nbBombeAutour++;
-          }
-          
-          if (y < gamesize-1) {
-            if ( tableauJeu[i][y-1]   == "B" ) nbBombeAutour++;
-          }
-          
-          if (i > 0) {
-            if ( tableauJeu[i-1][y]   == "B" ) nbBombeAutour++;
-          }
-          
-          if (i < gamesize-1) {
-            if ( tableauJeu[i+1][y]   == "B" ) nbBombeAutour++;
-          }
-        
-          if ( i > 0 && y > 0) {
-            if ( tableauJeu[i-1][y-1] == "B" ) nbBombeAutour++;
-          }
-        
-          if (i < gamesize -1 && y < gamesize-1 ) {
-
-            if ( tableauJeu[i+1][y+1] == "B" ) nbBombeAutour++;
-          }
-        
-          if (i > gamesize-1 && y > 0) {
-            if ( tableauJeu[i+1][y-1] == "B" ) nbBombeAutour++;
-          }
-        
-          if (i > 0 && y <  gamesize-1) {
-            if ( tableauJeu[i-1][y+1] == "B" ) nbBombeAutour++;
-          }
-        tableauJeu[i][y] = nbBombeAutour;
-        
-      }
-
-    }
-  }
-}
-
-function cheat() {
-  for (var i = 0; i < gamesize; i++) {
-    for (var y = 0; y < gamesize; y++) {
-      document.write(tableauJeu[i][y])
-    }
-    document.write("<br>")
-  }
-}
-
-// Init plateau;
-initBombe();
-
-//initNumbers();
-// Init refresh;
-cheat()
-
-
-// Restart game;
-
-
-const cases = document.querySelectorAll(".case");
-
-for(var i = 0; i < cases.length; i++) {
-  cases[i].addEventListener('click', event => {
-    //console.log(event.originalTarget)
-    revealAllCase(event.originalTarget)
-  });
-}
-
-function revealOneCase(row, col) {
-  var libCase;
-  var c = document.getElementById(`${row}-${col}`);
-  console.log("row : " + row + " col " + row)
-  if (tableauJeu[row][col] !== "B" && col < gamesize -1) {
-    switch (tableauJeu[row][col]) {
-      case "B" : libCase = "💣"; break;
-      case  1  : libCase = 1   ; c.style.color = "#44bd32"; break;
-      case  2  : libCase = 2   ; c.style.color = "#487eb0"; break;
-      case  3  : libCase = 3   ; c.style.color = "#273c75"; break;
-      case  4  : libCase = 4   ; c.style.color = "#ffa502"; break;
-      case  5  : libCase = 5   ; c.style.color = "#ff6348"; break;
-      case  6  : libCase = 6   ; c.style.color = "#e84118"; break;
-      case  7  : libCase = 7   ; c.style.color = "#e84118"; break;
-      case  8  : libCase = 8   ; c.style.color = "#e84118"; break;
-      default :  libCase = " " ; break;
-    }
-    c.innerHTML = libCase;
-    c.style.background = "#a4b0be";
-    
-    revealOneCase(row, col+1);
-  }
-
-}
-
-function revealAllCase(c) {
-    var row = c.id.split("-")[0];
-    var col = c.id.split("-")[1];
-    
-    if (tableauJeu[row][col] !== "B" ) {
-      revealOneCase(row, col);
-    }
-
-}
-
-function revealCase(c) {
-    var libCase;
-    var row = c.id.split("-")[0];
-    var col = c.id.split("-")[1];
-    switch (tableauJeu[row][col]) {
-      case "B" : libCase = "💣"; break;
-      case  1  : libCase = 1   ; c.style.color = "#44bd32"; break;
-      case  2  : libCase = 2   ; c.style.color = "#487eb0"; break;
-      case  3  : libCase = 3   ; c.style.color = "#273c75"; break;
-      case  4  : libCase = 4   ; c.style.color = "#ffa502"; break;
-      case  5  : libCase = 5   ; c.style.color = "#ff6348"; break;
-      case  6  : libCase = 6   ; c.style.color = "#e84118"; break;
-      case  7  : libCase = 7   ; c.style.color = "#e84118"; break;
-      case  8  : libCase = 8   ; c.style.color = "#e84118"; break;
-      default :  libCase = " " ;
-    }
-    c.innerHTML = libCase;
-    c.style.background = "#a4b0be"
-}
-
-function revealTable() {
-  for(var i = 0; i < cases.length; i++) {
-    revealCase(cases[i])
-  }
-}
-
-revealTable();
-
-// Drapeau
-window.oncontextmenu = function ()
-{
-      var x = event.clientX;
-      var y = event.clientY;
-      var elementMouseIsOver = document.elementFromPoint(x, y);
-      var elements = document.querySelectorAll(':hover');
-  
-      if (elements[4] !== 'undefined' ) {
-        if (elements[4].innerHTML == "🏳️") {
-          elements[4].innerHTML = "";
-          return false;
-        }
-
-        if (elements[4].innerHTML == "") {
-          elements[4].innerHTML = "🏳️";
-          return false;
-        }
-      }
-      return false;
-}
-
-
-
-
-function revealBomnbeArround(x, y) {
-    
-
-    if (tableauJeu[x][y].isMine) {
-        revealAll()
-        return;
-    }
-
-    if (tableauJeu[x][y].nbMines > 0) {
-        tableauJeu[x][y].isRevealed = true;
-        return;
-    }
-    else {
-      tableauJeu[x][y].isRevealed = true;
-      return;
-    }
-}
-
-*/
