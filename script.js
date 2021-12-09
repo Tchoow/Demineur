340aed5d3d77a7fae7e897220a7bfdf1cb57e1f3
Theo <45401519+Tchoow@users.noreply.github.com>
2021-12-09 16:29:15 +0100

    Ajout d'un pattern au background + top bar menu

--- script.js
+++ script.js
@@ -11,14 +11,16 @@ Github : https://github.com/Tchoow
 */
 
 
-// Ajout d'un event clique droit sur toute les cases
 // 🏳️
 // 💣
 
-const gamesize = 14;
-var tableauJeu = [];
-var nbCliques  =  0;
-
+const gamesize     = 14;
+var tableauJeu     = [];
+var nbCliques      = 0;
+var nbGameFlags    = 0;
+var nbGameBombes   = 0;
+var GameTimerValue = 000;
+var loopActualisation;
 
 for (var i = 0; i < gamesize; i++) tableauJeu[i] = [];
 const wrapper = document.querySelector('body');
@@ -37,9 +39,38 @@ function initDefaultTableJeu() {
     }
 }
 
+function updateTopBar() {
+   document.querySelector(".bombes").innerHTML = "💣 "+nbGameBombes;
+   document.querySelector(".flags").innerHTML = "🏳️ "+nbGameFlags;
+   GameTimerValue = GameTimerValue.toString();
+   while (GameTimerValue.length < 3) GameTimerValue = "0" + GameTimerValue;
+   document.querySelector(".timer").innerHTML = "⏱️ "+GameTimerValue;
+
+   GameTimerValue++;
+}
+
+function resetTopBar() {
+  nbGameBombes = nbGameFlags = GameTimerValue = 0;
+  document.querySelector(".bombes").innerHTML = "💣 --";
+  document.querySelector(".flags").innerHTML = "🏳️ --";
+  document.querySelector(".timer").innerHTML = "⏱️ ---";
+  
+  clearInterval(loopActualisation);
+}
+
+function initTopBar() {
+  loopActualisation = window.setInterval(function(){
+    updateTopBar()
+  }, 1000);
+}
+
+
+
 function initBombeTableJeu(row, col) {
     var nbBombe = Math.floor(gamesize * gamesize / 10);
     var nbBombePlaced = 0;
+    nbGameBombes = nbGameFlags = nbBombe;
+    initTopBar()
     while (nbBombePlaced < nbBombe) {
         var x = Math.floor(Math.random() * gamesize);
         var y = Math.floor(Math.random() * gamesize);
@@ -48,6 +79,8 @@ function initBombeTableJeu(row, col) {
             nbBombePlaced++;
         }
     }
+    try {
+
     tableauJeu[row][col].isMine  = false;
     tableauJeu[row][col].nbMines = 0;
     tableauJeu[row+1][col].isMine  = false;
@@ -66,6 +99,10 @@ function initBombeTableJeu(row, col) {
     tableauJeu[row+1][col-1].nbMines = 0;
     tableauJeu[row-1][col+1].isMine  = false;
     tableauJeu[row-1][col+1].nbMines = 0;
+    } catch (error) {
+      console.error(error);
+    }
+
 }
 
 function resetGame() {
@@ -73,9 +110,12 @@ function resetGame() {
   initDefaultTableJeu();
   resetStyleDefault();
   showTableJeu();
+  resetTopBar();
   
   // reset the color of the body
   wrapper.style.background = "#333333";
+  wrapper.style.backgroundImage ="url('data:image/svg+xml,%3Csvg width=\"52\" height=\"26\" viewBox=\"0 0 52 26\" xmlns=\"http://www.w3.org/2000/svg\"%3E%3Cg fill=\"none\" fill-rule=\"evenodd\"%3E%3Cg fill=\"%239C92AC\" fill-opacity=\"0.4\"%3E%3Cpath d=\"M10 10c0-2.21-1.79-4-4-4-3.314 0-6-2.686-6-6h2c0 2.21 1.79 4 4 4 3.314 0 6 2.686 6 6 0 2.21 1.79 4 4 4 3.314 0 6 2.686 6 6 0 2.21 1.79 4 4 4v2c-3.314 0-6-2.686-6-6 0-2.21-1.79-4-4-4-3.314 0-6-2.686-6-6zm25.464-1.95l8.486 8.486-1.414 1.414-8.486-8.486 1.414-1.414z\" /%3E%3C/g%3E%3C/g%3E%3C/svg%3E')";
+
 }
 
 
@@ -99,6 +139,7 @@ function isLoose() {
         if (tableauJeu[i][j].isMine && tableauJeu[i][j].isRevealed) {
             revealAll();
             wrapper.style.background = "#e74c3c";
+            clearInterval(loopActualisation);
             return true;
       }
 
@@ -254,12 +295,11 @@ for(var i = 0; i < cases.length; i++) {
     else {
       if (!tableauJeu[row][col].isFlagged) {
         revealBomnbeArround(row, col);
-        
         if ( ! isLoose() ) {
           if(isWin()) {
-            console.log("win")
             wrapper.style.background = "#27ae60";
             revealAll();
+            clearInterval(loopActualisation);
           }
         };
       }
@@ -280,10 +320,15 @@ for(var i = 0; i < cases.length; i++) {
     var row = parseInt(c[0]);
     var col = parseInt(c[1]);
     if (!tableauJeu[row][col].isFlagged) {
-      tableauJeu[row][col].isFlagged = true;
+      if (nbGameFlags > 0) {
+        tableauJeu[row][col].isFlagged = true;
+        nbGameFlags--;
+      }
+
     }
     else {
       tableauJeu[row][col].isFlagged = false;
+      nbGameFlags++;
     }
     
     showTableJeu();
